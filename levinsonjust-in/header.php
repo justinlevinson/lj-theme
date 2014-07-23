ae42911dd1e3555a14bb9490f3ec85925d3d8204
justinlevinson <justin@mkshft.org>
2014-07-23 17:34:25 -0400

    typeface shenanigans

--- levinsonjust-in/header.php
+++ levinsonjust-in/header.php
@@ -9,7 +9,8 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<link href='http://fonts.googleapis.com/css?family=Lato:400,900' rel='stylesheet' type='text/css'>
+<?php /* <link href='http://fonts.googleapis.com/css?family=Lato:400,900' rel='stylesheet' type='text/css'> */ ?>
+<link href='http://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php wp_title( '|', true, 'right' ); ?></title>
